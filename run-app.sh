6080a3a29b7d3a5fea534cb87a37ca2c5fb4189b
IsmailKarakayaa <47555011+IsmailKarakayaa@users.noreply.github.com>
2022-08-12 22:09:15 +0200

    Add command to forward to stderr

--- run-app.sh
+++ run-app.sh
@@ -3,4 +3,4 @@
 set -euo pipefail
 
 docker build -t webapp .
-docker run -t -d -p 80:80 --network mssql_default --name SportStoreWebApp webapp
+docker run -t -d -p 80:80 --network mssql_default --name SportStoreWebApp webapp >2 ~/SportStoreLog.log
