11e58272c570359e3c3bd787efcf329dc1cd61cf
IsmailKarakayaa <47555011+IsmailKarakayaa@users.noreply.github.com>
2022-08-12 21:35:10 +0200

    Update script to create image and run container
    Updated script so it creates an image from the dockerfile and starts the container

--- run-app.sh
+++ run-app.sh
@@ -2,6 +2,5 @@
 
 set -euo pipefail
 
-# dotnet build
-docker-compose build
-docker-compose up
+docker build -d WebApp .
+docker run -t -p 80:80 --network mssql_default --name SportStoreWebApp WebApp
