b3439a9164247ac68420b9e9fa2d016248818477
IsmailKarakayaa <47555011+IsmailKarakayaa@users.noreply.github.com>
2022-08-17 20:14:28 +0200

    Fix >2 to 2>

--- run-app.sh
+++ run-app.sh
@@ -3,4 +3,4 @@
 set -euo pipefail
 
 docker build -t webapp .
-docker run -t -d -p 80:80 --network mssql_default --name SportStoreWebApp webapp >2 ~/SportStoreLog.log
+docker run -t -d -p 80:80 --network mssql_default --name SportStoreWebApp webapp 2> ~/SportStoreLog.log
