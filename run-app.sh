a392266f4f1c7a6f519dff460fcd9192f2dcfc1e
IsmailKarakayaa <47555011+IsmailKarakayaa@users.noreply.github.com>
2022-08-17 22:26:04 +0200

    Add certificate file .pfx

--- run-app.sh
+++ run-app.sh
@@ -2,7 +2,7 @@
 
 set -euo pipefail
 
-dotnet dev-certs https -ep /etc/ssl/sportstore/ -p Password
+dotnet dev-certs https -ep /etc/ssl/sportstore/sportstore.pfx -p Password
 
 docker build -t webapp .
 
